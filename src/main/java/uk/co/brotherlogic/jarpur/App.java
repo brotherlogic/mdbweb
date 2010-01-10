dd0cc2f822f57f966c2af4f7e6c8a51b6ab0261a
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-01-10 09:09:53 +0000

    Got simple framework working

--- src/main/java/uk/co/brotherlogic/jarpur/App.java
+++ src/main/java/uk/co/brotherlogic/jarpur/App.java
@@ -1,19 +1,36 @@
 package uk.co.brotherlogic.jarpur;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class App extends HttpServlet {
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 "
-				+ "Transitional//EN\">\n" + "<html>\n"
-				+ "<head><title>Hello WWW</title></head>\n" + "<body>\n"
-				+ "<h1>Hello WWW</h1>\n" + "</body></html>");
+public class App extends HttpServlet
+{
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+
+		res.setContentType("text/html");
+
+		PrintWriter out = res.getWriter();
+
+		/* Display some response to the user */
+
+		out.println("<html><head>");
+		out.println("<title>TestServlet</title>");
+		out.println("\t<style>body { font-family: 'Lucida Grande', " + "'Lucida Sans Unicode';font-size: 13px; }</style>");
+		out.println("</head>");
+		out.println("<body>");
+
+		out.println("<p>Current Date/Time: " + Calendar.getInstance().getTimeInMillis() + "</p>");
+		out.println("</body></html>");
+
+		out.close();
+
 	}
 }
