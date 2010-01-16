06d326112cb4285d75ded92b472c77cc90b4f3c7
Simon Home <simon@hysear.(none)>
2010-01-16 09:53:02 +0000

    Updated the record page

--- src/main/java/uk/co/brotherlogic/jarpur/Page.java
+++ src/main/java/uk/co/brotherlogic/jarpur/Page.java
@@ -7,27 +7,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public abstract class Page
-{
-	protected abstract String buildPage(Map<String, String> params) throws IOException;
+public abstract class Page {
+	protected abstract String buildPage(Map<String, String> params)
+			throws IOException;
 
 	Pattern percMatcher = Pattern.compile("(\\%\\%.*?\\%\\%)");
 
-	protected String buildPageFromTemplate(Map<String, Object> paramMap) throws IOException
-	{
+	DateFormat df = DateFormat.getDateInstance();
+
+	protected String buildPageFromTemplate(Map<String, Object> paramMap)
+			throws IOException {
 		String className = this.getClass().getCanonicalName();
 
 		// Build the template
 		StringBuffer template_data = new StringBuffer();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(FrontOfHouse.context.getRealPath("WEB-INF")
-				+ "/" + className.replace(".", "/") + ".html"))));
-		for (String line = reader.readLine(); line != null; line = reader.readLine())
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(new File(FrontOfHouse.context
+						.getRealPath("WEB-INF")
+						+ "/" + className.replace(".", "/") + ".html"))));
+		for (String line = reader.readLine(); line != null; line = reader
+				.readLine())
 			template_data.append(line);
 
 		// Apply the template
@@ -36,45 +43,33 @@ public abstract class Page
 		return template_data.toString();
 	}
 
-	protected Object resolveMethod(Object obj, String methodName)
-	{
-		try
-		{
+	protected Object resolveMethod(Object obj, String methodName) {
+		try {
 			Class cls = obj.getClass();
 			Method m = cls.getMethod(methodName, new Class[0]);
 			Object ret = m.invoke(obj, new Object[0]);
 			return ret;
-		}
-		catch (NoSuchMethodException e)
-		{
+		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
-		}
-		catch (IllegalArgumentException e)
-		{
+		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
-		}
-		catch (IllegalAccessException e)
-		{
+		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		}
-		catch (InvocationTargetException e)
-		{
+		} catch (InvocationTargetException e) {
 			e.printStackTrace();
 		}
 
 		return null;
 	}
 
-	protected Object resolve(String replace, Map<String, Object> paramMap)
-	{
+	protected Object resolve(String replace, Map<String, Object> paramMap) {
 
 		String[] elems = replace.split("\\.");
-		if (paramMap.containsKey(elems[0]))
-		{
+		if (paramMap.containsKey(elems[0])) {
 			Object obj = paramMap.get(elems[0]);
-			for (int i = 1; i < elems.length; i++)
-			{
-				System.err.println("START = " + obj + "(" + obj.getClass() + ")");
+			for (int i = 1; i < elems.length; i++) {
+				System.err.println("START = " + obj + "(" + obj.getClass()
+						+ ")");
 				obj = resolveMethod(obj, elems[i]);
 				System.err.println("END = " + obj + "(" + obj.getClass() + ")");
 			}
@@ -87,8 +82,8 @@ public abstract class Page
 
 	Pattern forPattern = Pattern.compile("for (.*?) in (.*?)%");
 
-	protected StringReplace replaceWithForLoop(StringBuffer buffer, int startPoint, int endPoint, Map<String, Object> paramMap)
-	{
+	protected StringReplace replaceWithForLoop(StringBuffer buffer,
+			int startPoint, int endPoint, Map<String, Object> paramMap) {
 		// Find the end of the for loop
 		int startOfEnd = buffer.lastIndexOf("%%endfor%%");
 		String replacementText = buffer.substring(endPoint, startOfEnd);
@@ -96,17 +91,19 @@ public abstract class Page
 		String replaceString = "";
 
 		// Get the properties of the for loop
-		Matcher forMatch = forPattern.matcher(buffer.substring(startPoint, endPoint));
-		if (forMatch.find())
-		{
+		Matcher forMatch = forPattern.matcher(buffer.substring(startPoint,
+				endPoint));
+		if (forMatch.find()) {
 			String newParam = forMatch.group(1);
 			String collection = forMatch.group(2);
 
-			System.err.println("FROM: " + buffer.substring(startPoint, endPoint));
-			System.err.println("Building collection = " + collection + " and " + newParam);
-			Collection<Object> objects = (Collection<Object>) resolve(collection, paramMap);
-			for (Object object : objects)
-			{
+			System.err.println("FROM: "
+					+ buffer.substring(startPoint, endPoint));
+			System.err.println("Building collection = " + collection + " and "
+					+ newParam);
+			Collection<Object> objects = (Collection<Object>) resolve(
+					collection, paramMap);
+			for (Object object : objects) {
 				StringBuffer temp = new StringBuffer(replacementText);
 				paramMap.put(newParam, object);
 				doReplace(temp, paramMap);
@@ -121,56 +118,57 @@ public abstract class Page
 
 	}
 
-	protected void doReplace(StringBuffer buffer, Map<String, Object> paramMap)
-	{
+	protected void doReplace(StringBuffer buffer, Map<String, Object> paramMap) {
 		// Simple replace
 		Matcher matcher = percMatcher.matcher(buffer.toString());
 		Stack<StringReplace> replaceStack = new Stack<StringReplace>();
 		int matchPoint = 0;
-		while (matcher.find(matchPoint))
-		{
+		while (matcher.find(matchPoint)) {
 			int grpStart = matcher.start(1);
 			int grpEnd = matcher.end(1);
 
 			// Check that this isn't a for loop
-			if (buffer.substring(grpStart + 2, grpEnd - 2).startsWith("for "))
-			{
-				replaceStack.push(replaceWithForLoop(buffer, grpStart, grpEnd, paramMap));
-			}
-			else
-			{
-				String replaceText = resolve(buffer.substring(grpStart + 2, grpEnd - 2), paramMap).toString();
-				replaceStack.push(new StringReplace(grpStart, grpEnd, replaceText));
+			if (buffer.substring(grpStart + 2, grpEnd - 2).startsWith("for ")) {
+				replaceStack.push(replaceWithForLoop(buffer, grpStart, grpEnd,
+						paramMap));
+			} else {
+				String replaceText = convert(resolve(buffer.substring(
+						grpStart + 2, grpEnd - 2), paramMap));
+				replaceStack.push(new StringReplace(grpStart, grpEnd,
+						replaceText));
 			}
 
 			matchPoint = replaceStack.peek().getEnd();
 		}
 
-		while (!replaceStack.isEmpty())
-		{
+		while (!replaceStack.isEmpty()) {
 			replaceStack.pop().apply(buffer);
 		}
 	}
+
+	protected String convert(Object obj) {
+		System.err.println(obj.getClass() + " is the type");
+		if (obj instanceof Calendar)
+			return df.format(((Calendar) obj).getTime());
+		else
+			return obj.toString();
+	}
 }
 
-class StringReplace
-{
+class StringReplace {
 	int start;
 	int end;
 	String toPlace;
 
-	public void apply(StringBuffer buffer)
-	{
+	public void apply(StringBuffer buffer) {
 		buffer.replace(start, end, toPlace);
 	}
 
-	public int getEnd()
-	{
+	public int getEnd() {
 		return end;
 	}
 
-	public StringReplace(int st, int en, String pl)
-	{
+	public StringReplace(int st, int en, String pl) {
 		start = st;
 		end = en;
 		toPlace = pl;
