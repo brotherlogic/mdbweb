eb13a5bf9cabdc134b98d22fd8d5ac122e09ea11
Simon Home <simon@hysear.(none)>
2010-02-14 17:12:46 +0000

    Modified the speed test

--- src/test/java/uk/co/brotherlogic/jarpu/SpeedTest.java
+++ src/test/java/uk/co/brotherlogic/jarpu/SpeedTest.java
@@ -1,53 +1,37 @@
 package uk.co.brotherlogic.jarpu;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Map;
-import java.util.Properties;
-import java.util.TreeMap;
-
 import junit.framework.TestCase;
-import uk.co.brotherlogic.jarpur.LinkTable;
-import uk.co.brotherlogic.jarpur.Page;
-import uk.co.brotherlogic.jarpur.replacers.SimpleReplacer;
-import uk.co.brotherlogic.mdb.Connect;
-import uk.co.brotherlogic.mdbweb.record.RecordPage;
 
-public class SpeedTest extends TestCase {
+public class SpeedTest extends TestCase
+{
 
-	public void testSpeed() {
+	public void testSpeed()
+	{
 
-		try {
-			// Build the record page for the Girl Group Sound - Lost and Found
-			long sTime = System.currentTimeMillis();
-			Properties lProps = new Properties();
-			lProps.load(new FileInputStream(new File("src/main/webapp/WEB-INF"
-					+ "/links.properties")));
-			SimpleReplacer.setLinkTable(new LinkTable(lProps));
-
-			Page rPage = new RecordPage();
-			Map<String, String> objMap = new TreeMap<String, String>();
-			objMap.put("id", "6398");
-			rPage.buildPage(objMap);
-
-			long eTime = System.currentTimeMillis() - sTime;
-
-			long dTime = Connect.getConnection().getTQueryTime();
-
-			Connect.getConnection().printStats();
-			System.err.println("Database calls = " + dTime + " ("
-					+ Connect.getConnection().getSCount() + ")");
-			System.err.println("Time to build page = " + (eTime - dTime));
-		} catch (IOException e) {
-			e.printStackTrace();
-			assert (false);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		assert (true);
+		/*
+		 * try { // Build the record page for the Girl Group Sound - Lost and
+		 * Found /*long sTime = System.currentTimeMillis(); Properties lProps =
+		 * new Properties(); lProps.load(new FileInputStream(new
+		 * File("src/main/webapp/WEB-INF" + "/links.properties")));
+		 * SimpleReplacer.setLinkTable(new LinkTable(lProps));
+		 * 
+		 * Page rPage = new RecordPage(); Map<String, String> objMap = new
+		 * TreeMap<String, String>(); objMap.put("id", "6398");
+		 * rPage.buildPage(objMap);
+		 * 
+		 * long eTime = System.currentTimeMillis() - sTime;
+		 * 
+		 * long dTime = Connect.getConnection().getTQueryTime();
+		 * 
+		 * Connect.getConnection().printStats();
+		 * System.err.println("Database calls = " + dTime + " (" +
+		 * Connect.getConnection().getSCount() + ")");
+		 * System.err.println("Time to build page = " + (eTime - dTime)); }
+		 * catch (IOException e) { e.printStackTrace(); assert (false); } catch
+		 * (SQLException e) { e.printStackTrace(); }
+		 * 
+		 * assert (true);
+		 */
 
 	}
 }
