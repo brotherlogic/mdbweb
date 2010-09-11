bf219c60579b4916507a93cbfe5c5dae380f1ae3
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-09-11 08:53:31 +0100

    Fixed time display

--- src/main/java/uk/co/brotherlogic/mdbweb/record/Default.java
+++ src/main/java/uk/co/brotherlogic/mdbweb/record/Default.java
@@ -15,13 +15,16 @@ import uk.co.brotherlogic.mdb.record.GetRecords;
 import uk.co.brotherlogic.mdb.record.Record;
 import uk.co.brotherlogic.mdb.record.Track;
 
-public class Default extends TemplatePage {
+public class Default extends TemplatePage
+{
 	@Override
 	protected Map<String, Object> convertParams(List<String> elems,
-			Map<String, String> params) {
+			Map<String, String> params)
+	{
 		Map<String, Object> paramMap = new TreeMap<String, Object>();
 
-		try {
+		try
+		{
 			int recordID = Integer.parseInt(elems.get(0));
 			Record record = GetRecords.create().getRecord(recordID);
 
@@ -30,36 +33,73 @@ public class Default extends TemplatePage {
 			paramMap.put("sscore", record.getScore(User.getUser("simon")));
 			paramMap.put("jscore", record.getScore(User.getUser("jeanette")));
 
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 
 		return paramMap;
 	}
 
+	public String convertTime(int timeIn)
+	{
+		if (timeIn <= 0)
+			return "";
+
+		int hours = timeIn / (60 * 60);
+		int minutes = (timeIn - hours * 60 * 60) / 60;
+		int seconds = (timeIn - hours * 60 * 60 - minutes * 60);
+
+		if (hours > 0)
+			return hours + ":" + pad(minutes) + ":" + pad(seconds);
+		else
+			return pad(minutes) + ":" + pad(seconds);
+	}
+
 	@Override
-	public Class generates() {
+	public Class generates()
+	{
 		return Record.class;
 	}
 
 	@Override
-	public String linkParams(Object arg0) {
+	public String linkParams(Object arg0)
+	{
 		return "" + (((Record) arg0).getNumber());
 	}
 
-	public Boolean relatedExists(Track track) {
-		try {
+	private String pad(int num)
+	{
+		if (num > 9)
+			return "" + num;
+		else
+			return "0" + num;
+	}
+
+	public Boolean relatedExists(Track track)
+	{
+		try
+		{
 
 			return GetRecords.create().getRecordsWithTrack(track.getTitle())
 					.size() > 1;
-		} catch (SQLException e) {
+		}
+		catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 		return false;
 	}
 
+	public String resolve(String number)
+	{
+		return number;
+	}
+
 	private Map<String, List<Artist>> splitArtists(Record rec)
-			throws SQLException {
+			throws SQLException
+	{
 		Map<String, List<Artist>> ret = new TreeMap<String, List<Artist>>();
 
 		// First create a map from personnel to tracks
@@ -74,7 +114,8 @@ public class Default extends TemplatePage {
 							.getTrackNumber()));
 
 		// Now transform into the return set
-		for (Entry<Artist, String> arts : persToTracks.entrySet()) {
+		for (Entry<Artist, String> arts : persToTracks.entrySet())
+		{
 			if (!ret.containsKey(arts.getValue()))
 				ret.put(arts.getValue(), new LinkedList<Artist>());
 			ret.get(arts.getValue()).add(arts.getKey());
